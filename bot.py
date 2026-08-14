e59ce52d19bf3bb99b182d1675ad5345dbab7801
gleb4ikbloger-cell <gleb4ikbloger@gmail.com>
2026-08-14 22:52:05 +0300

    Implement health check and web server
    Added a health check endpoint and started a web server.

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import os
 import asyncio
+from aiohttp import web
 
 from aiogram import Bot, Dispatcher
 from aiogram.types import Message
@@ -7,6 +8,7 @@ from aiogram.filters import CommandStart
 
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
+PORT = int(os.getenv("PORT", 10000))
 
 if not BOT_TOKEN:
     raise RuntimeError("BOT_TOKEN не найден")
@@ -24,8 +26,30 @@ async def start_handler(message: Message):
     )
 
 
+async def health_check(request):
+    return web.Response(text="PaltoPay is running!")
+
+
+async def start_web_server():
+    app = web.Application()
+    app.router.add_get("/", health_check)
+
+    runner = web.AppRunner(app)
+    await runner.setup()
+
+    site = web.TCPSite(runner, "0.0.0.0", PORT)
+    await site.start()
+
+    print(f"HTTP сервер запущен на порту {PORT}")
+
+
 async def main():
+    print("PaltoPay запускается...")
+
+    await start_web_server()
+
     print("PaltoPay запущен!")
+
     await dp.start_polling(bot)
 
 
